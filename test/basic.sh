4cdc1f2f5b30c526cdca2037e79c26aee1eea9d9
Joachim Wiberg <troglobit@gmail.com>
2021-11-21 09:16:06 +0100

    test: improve basic test slighty w/ 2nd interface and more addresses
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- test/basic.sh
+++ test/basic.sh
@@ -9,23 +9,29 @@ print "Check deps ..."
 check_dep tshark
 
 print "Creating world ..."
-for iface in eth0 eth1 eth3; do
+for iface in eth0 eth1 eth3 eth4; do
     ip link add $iface type dummy
     ip link set $iface up
     ip link set $iface multicast on
 done
-ip addr add 192.168.1.1/24 dev eth0
+ip addr add 192.168.0.1/24 dev eth0
+ip addr add 192.168.1.1/24 dev eth1
+ip addr add 192.168.1.2/24 dev eth1
+ip addr add 192.168.3.1/24 dev eth1
+ip addr add 192.168.2.1/24 dev eth2
 
 ip -br l
 ip -br a
 
 print "Creating config ..."
 cat <<EOF > "/tmp/$NM/config"
-eth0
+eth0 eth1
 EOF
 
 print "Starting collector(s) ..."
-tshark -lni eth0 -w "/tmp/$NM/pcap" 2>/dev/null &
+tshark -lni eth0 -w "/tmp/$NM/eth0.pcap" 2>/dev/null &
+echo $! >> "/tmp/$NM/PIDs"
+tshark -lni eth1 -w "/tmp/$NM/eth1.pcap" 2>/dev/null &
 echo $! >> "/tmp/$NM/PIDs"
 sleep 1
 
@@ -37,11 +43,13 @@ sleep 2
 kill_pids
 
 print "Analyzing pcap ..."
-lines=$(tshark -n -r "/tmp/$NM/pcap" 2>/dev/null | grep "IGMPv3 50 Membership Query" | tee -a "/tmp/$NM/result" | wc -l)
+lines1=$(tshark -n -r "/tmp/$NM/eth0.pcap" 2>/dev/null | grep "IGMPv3 50 Membership Query" | tee -a "/tmp/$NM/result" | wc -l)
+lines2=$(tshark -n -r "/tmp/$NM/eth1.pcap" 2>/dev/null | grep "IGMPv3 50 Membership Query" | tee -a "/tmp/$NM/result" | wc -l)
 cat "/tmp/$NM/result"
 
-echo " => $lines IGMP Query, expected 1"
+echo " => $lines1 IGMP Query on eth0, expected 1"
+echo " => $lines2 IGMP Query on eth1, expected 1"
 # shellcheck disable=SC2086 disable=SC2166
-[ $lines -eq 1 ] || FAIL
+[ $lines1 -eq 1 -a $lines2 -eq 1 ] || FAIL
 
 OK
